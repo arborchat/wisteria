1f177d1e6f8cfdf5b7e605d4d25d141293c5a654
Chris Waldon <christopher.waldon.dev@gmail.com>
2020-01-30 18:21:20 -0500

    feat(config): use new config file name
    This allows parallel installations of this version of wisteria
    and the one that operates on the older verison of forest nodes.

--- config.go
+++ config.go
@@ -73,7 +73,7 @@ func DefaultConfigFilePath() (string, error) {
 		return "", fmt.Errorf("failed looking up configuration dir: %w", err)
 	}
 	const wisteriaConfigDirName = "wisteria"
-	const wisteriaConfigFileNameJSON = "wisteria.json"
+	const wisteriaConfigFileNameJSON = "wisteria-2.json"
 	configFile := filepath.Join(configDir, wisteriaConfigDirName, wisteriaConfigFileNameJSON)
 	return configFile, nil
 }
