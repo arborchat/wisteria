f8d42fb226d1f30fcb033e3f2dc435c284e1fef4
Chris Waldon <christopher.waldon.dev@gmail.com>
2020-02-05 19:41:59 -0500

    feat: use in-interface editor by default
    Now `i`, `c`, and `Enter` all conjure up the ephemeral builtin
    editor to send messages. You can access the external editor using
    `I` and `C`.

--- history_widget.go
+++ history_widget.go
@@ -7,14 +7,58 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 
 	forest "git.sr.ht/~whereswaldon/forest-go"
+	"git.sr.ht/~whereswaldon/wisteria/archive"
 	"git.sr.ht/~whereswaldon/wisteria/widgets"
 	"github.com/0xAX/notificator"
 	wrap "github.com/bbrks/wrap/v2"
 	"github.com/gdamore/tcell"
 	"github.com/gdamore/tcell/views"
 )
+
+// EditRequestMap tracks the outstanding edit requests (messages that
+// are in the process of being replied to) and which nodes those replies
+// are intended for. It provides concurrent-safe operations to get and ID
+// for a replying to a particular node as well as for getting the node
+// back from its ID.
+type EditRequestMap struct {
+	Requests map[int]forest.Node
+	sync.Mutex
+	current int
+}
+
+// NewEditRequestMap initializes a new map
+func NewEditRequestMap() *EditRequestMap {
+	return &EditRequestMap{
+		Requests: make(map[int]forest.Node),
+	}
+}
+
+// Insert chooses an ID for the node and stores the node with that ID.
+// It returns the chosen ID.
+func (e *EditRequestMap) Insert(node forest.Node) int {
+	e.Lock()
+	defer e.Unlock()
+	i := e.current
+	e.current++
+	e.Requests[i] = node
+	return i
+}
+
+// Delete retrieves the node associated with the given ID by removing
+// it from the map.
+func (e *EditRequestMap) Delete(id int) forest.Node {
+	e.Lock()
+	defer e.Unlock()
+	node, present := e.Requests[id]
+	if !present {
+		return nil
+	}
+	delete(e.Requests, id)
+	return node
+}
 
 // HistoryWidget is the controller for the chat history TUI
 type HistoryWidget struct {
@@ -24,6 +68,34 @@ type HistoryWidget struct {
 	*forest.Builder
 	*Config
 	*notificator.Notificator
+	*EditRequestMap
+}
+
+func NewHistoryWidget(app *views.Application, archive *archive.Archive, config *Config, notifier *notificator.Notificator) (*HistoryWidget, error) {
+	hv := &HistoryView{
+		Archive: archive,
+	}
+	if err := hv.Render(); err != nil {
+		return nil, fmt.Errorf("failed initializing history view: %w", err)
+	}
+	cv := NewCellView()
+	cv.SetModel(hv)
+	cv.MakeCursorVisible()
+	hv.SelectLastLine()
+
+	builder, err := config.Builder(archive)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating node builder for widget: %w", err)
+	}
+	return &HistoryWidget{
+		HistoryView:    hv,
+		CellView:       cv,
+		Application:    app,
+		Builder:        builder,
+		Config:         config,
+		Notificator:    notifier,
+		EditRequestMap: NewEditRequestMap(),
+	}, nil
 }
 
 var _ views.Widget = &HistoryWidget{}
@@ -86,14 +158,61 @@ func (v *HistoryWidget) TryNotify(reply *forest.Reply) {
 // StartReply begins a new reply with the currently-selected message as its
 // parent.
 func (v *HistoryWidget) StartReply() error {
+	reply, msg, err := v.CurrentReplyConfig()
+	if err != nil {
+		return fmt.Errorf("failed getting current reply configuration: %w", err)
+	}
+	return v.StartNewNode(reply, msg)
+}
+
+func (v *HistoryWidget) CurrentReplyConfig() (forest.Node, string, error) {
 	reply, err := v.CurrentReply()
 	if err != nil {
-		return fmt.Errorf("couldn't determine current reply: %v", err)
+		return nil, "", fmt.Errorf("couldn't determine current reply: %v", err)
 	}
 	msg := strings.Join(strings.Split(string(reply.Content.Blob), "\n"), "\n#")
 	msg = fmt.Sprintf("# replying to %s\n", msg)
-	editReq := widgets.NewEventEditRequest(0, v, msg)
+	return reply, msg, nil
+}
+
+func (v *HistoryWidget) NewConversationConfig() (forest.Node, string, error) {
+	reply, err := v.CurrentReply()
+	if err != nil {
+		return nil, "", fmt.Errorf("couldn't determine current reply: %w", err)
+	}
+	community, _, err := v.GetCommunity(&reply.CommunityID)
+	if err != nil {
+		return nil, "", fmt.Errorf("couldn't locate current community: %w", err)
+	}
+	msg := fmt.Sprintf("# starting new conversation in %s\n", string(community.(*forest.Community).Name.Blob))
+	return community, msg, nil
+}
+
+func (v *HistoryWidget) EmitReplyRequest() error {
+	reply, msg, err := v.CurrentReplyConfig()
+	if err != nil {
+		return fmt.Errorf("failed getting current reply configuration: %w", err)
+	}
+	v.EmitEditorRequest(reply, msg)
+	return nil
+}
+
+func (v *HistoryWidget) EmitConversationRequest() error {
+	community, msg, err := v.NewConversationConfig()
+	if err != nil {
+		return fmt.Errorf("failed getting current reply configuration: %w", err)
+	}
+	v.EmitEditorRequest(community, msg)
+	return nil
+}
+
+func (v *HistoryWidget) EmitEditorRequest(parent forest.Node, startText string) {
+	editReq := widgets.NewEventEditRequest(v.EditRequestMap.Insert(parent), v, startText)
 	v.PostEvent(editReq)
+}
+
+// StartNewNode launches an Editor to write and send a new arbor node.
+func (v *HistoryWidget) StartNewNode(parent forest.Node, startText string) error {
 	file, err := ioutil.TempFile("", "arbor-msg")
 	if err != nil {
 		return fmt.Errorf("couldn't create temporary file for reply: %v", err)
@@ -101,7 +220,7 @@ func (v *HistoryWidget) StartReply() error {
 	// ensure this file descriptor is closed
 	file.Close()
 	// populate the file, but keep it closed
-	err = ioutil.WriteFile(file.Name(), []byte(msg), 0660)
+	err = ioutil.WriteFile(file.Name(), []byte(startText), 0660)
 	if err != nil {
 		return fmt.Errorf("couldn't write template into temporary file: %v", err)
 	}
@@ -109,40 +228,23 @@ func (v *HistoryWidget) StartReply() error {
 	if err := editor.Start(); err != nil {
 		return fmt.Errorf("failed to start editor command: %v", err)
 	}
-	go v.FinishReply(reply, file.Name(), editor)
+	go v.FinishReply(parent, file.Name(), editor)
 	return nil
 }
 
 // StartConversation begins a new conversation in the same community as the
 // currently-selected message.
 func (v *HistoryWidget) StartConversation() error {
-	reply, err := v.CurrentReply()
+	community, msg, err := v.NewConversationConfig()
 	if err != nil {
-		return fmt.Errorf("couldn't determine current reply: %w", err)
+		return fmt.Errorf("couldn't get current community config: %w", err)
 	}
-	community, _, err := v.GetCommunity(&reply.CommunityID)
-	if err != nil {
-		return fmt.Errorf("couldn't locate current community: %w", err)
-	}
-	file, err := ioutil.TempFile("", "arbor-msg")
-	if err != nil {
-		return fmt.Errorf("couldn't create temporary file for reply: %v", err)
-	}
-	// ensure this file descriptor is closed
-	file.Close()
-	// populate the file, but keep it closed
-	err = ioutil.WriteFile(file.Name(), []byte(fmt.Sprintf("# starting new conversation in %s\n", string(community.(*forest.Community).Name.Blob))), 0660)
-	if err != nil {
-		return fmt.Errorf("couldn't write template into temporary file: %v", err)
-	}
-	editor := v.Config.EditFile(file.Name())
-	if err := editor.Start(); err != nil {
-		return fmt.Errorf("failed to start editor command: %v", err)
-	}
-	go v.FinishReply(community, file.Name(), editor)
-	return nil
+	return v.StartNewNode(community, msg)
 }
 
+// FinishReply waits for the provided editor command to complete (it is expected
+// to have already started) and writes the contents of the named file as a new
+// node.
 func (v *HistoryWidget) FinishReply(parent forest.Node, replyFileName string, editor *exec.Cmd) {
 	if err := editor.Wait(); err != nil {
 		log.Printf("Error waiting on editor command to finish: %v", err)
@@ -154,32 +256,35 @@ func (v *HistoryWidget) FinishReply(parent forest.Node, replyFileName string, ed
 		log.Printf("Error reading reply from %s: %v", replyFileName, err)
 		return
 	}
-	wrapper := wrap.NewWrapper()
-	wrapper.Breakpoints = " "
-	replyContentString := strings.Trim(wrapper.Wrap(stripCommentLines(string(replyContent)), 80), "\n")
-	if len(replyContentString) == 0 {
-		log.Println("Message is empty, not sending")
-		return
-	}
-	reply, err := v.NewReply(parent, replyContentString, "")
-	if err != nil {
-		log.Printf("Error creating reply: %v", err)
-		return
-	}
-	outfile, err := reply.ID().MarshalString()
-	if err != nil {
-		log.Printf("Error finding ID for reply: %v", err)
-		return
-	}
-	err = saveAs(outfile, reply)
-	if err != nil {
-		log.Printf("Error saving to %s: %v", outfile, err)
+	if err := v.FinishReplyString(parent, string(replyContent)); err != nil {
+		log.Printf("Error creating & sending reply: %w", err)
 		return
 	}
 	if err := os.Remove(replyFileName); err != nil {
 		log.Printf("Error removing %s: %v", replyFileName, err)
 		return
 	}
+}
+
+// FinishReplyString writes the content provided as the content of a new forest
+// node into the store.
+func (v *HistoryWidget) FinishReplyString(parent forest.Node, content string) error {
+	wrapper := wrap.NewWrapper()
+	wrapper.Breakpoints = " "
+	replyContentString := strings.Trim(wrapper.Wrap(stripCommentLines(content), 80), "\n")
+	if len(replyContentString) == 0 {
+		return fmt.Errorf("not sending empty message")
+	}
+	reply, err := v.NewReply(parent, replyContentString, "")
+	if err != nil {
+		return fmt.Errorf("failed creating reply: %w", err)
+	}
+	outfile := reply.ID().String()
+	err = saveAs(outfile, reply)
+	if err != nil {
+		return fmt.Errorf("failed saving reply to disk: %w", err)
+	}
+	return nil
 }
 
 func (v *HistoryWidget) HandleEvent(event tcell.Event) bool {
@@ -189,10 +294,13 @@ func (v *HistoryWidget) HandleEvent(event tcell.Event) bool {
 	switch keyEvent := event.(type) {
 	case widgets.EventEditFinished:
 		log.Printf("Got event edit finished: %v", keyEvent)
+		if err := v.FinishReplyString(v.EditRequestMap.Delete(keyEvent.ID), keyEvent.Content); err != nil {
+			log.Printf("Failed finalizing reply: %v", err)
+		}
 	case *tcell.EventKey:
 		switch keyEvent.Key() {
 		case tcell.KeyEnter:
-			if err := v.StartReply(); err != nil {
+			if err := v.EmitReplyRequest(); err != nil {
 				log.Printf("Error starting reply: %v", err)
 				return true
 			}
@@ -227,7 +335,22 @@ func (v *HistoryWidget) HandleEvent(event tcell.Event) bool {
 			v.MakeCursorVisible()
 			return true
 		case 'c':
+			if err := v.EmitConversationRequest(); err != nil {
+				log.Printf("Error starting conversation: %v", err)
+				return true
+			}
+		case 'C':
 			if err := v.StartConversation(); err != nil {
+				log.Printf("Error starting conversation: %v", err)
+				return true
+			}
+		case 'i':
+			if err := v.EmitReplyRequest(); err != nil {
+				log.Printf("Error starting conversation: %v", err)
+				return true
+			}
+		case 'I':
+			if err := v.StartReply(); err != nil {
 				log.Printf("Error starting conversation: %v", err)
 				return true
 			}
