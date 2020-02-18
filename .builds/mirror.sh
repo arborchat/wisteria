9aee920825125c4975a5f1f57c4696053e6da692
Chris Waldon <christopher.waldon.dev@gmail.com>
2020-02-17 22:16:22 -0500

    ci/cd: move release logic into separate script

--- .builds/mirror.sh
+++ .builds/mirror.sh
@@ -5,24 +5,19 @@ if [ "$#" -lt 1 ]; then
   echo "usage: mirror.sh <github_ssh_clone_url>" 1>&2
 fi
 
+basedir=$(dirname "$(realpath "$0")")
+
 GITHUB_MIRROR_URL="$1"
 
 # determine current commit
 GIT_COMMIT="$(git rev-parse HEAD)"
 
-ensure_ssh() {
-  # ensure our SSH allows us to talk to GitHub.
-  # Even if we ended up talking to a malicious host, all we'd do is push our
-  # open source code to them? Not really concerned about that.
-  KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
-  touch "$KNOWN_HOSTS_FILE"
-  chmod 0600 "$KNOWN_HOSTS_FILE"
-  ssh-keyscan github.com > "$KNOWN_HOSTS_FILE"
-}
+# shellcheck source=.builds/lib.sh
+. "$basedir/lib.sh"
 
 # if our current commit is on the master branch, push to GitHub
 mirror_branch="master"
-if git branch --format '%(refname:lstrip=2)' --contains "$GIT_COMMIT" | grep "$mirror_branch"; then
+if commit_on_branch "$GIT_COMMIT" "$mirror_branch"; then
   ensure_ssh
   remote_name="mirror"
   git remote add "$remote_name" "$GITHUB_MIRROR_URL" && \
---
+++ .builds/release.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+basedir=$(dirname "$(realpath "$0")")
+
+# determine current commit
+GIT_COMMIT="$(git rev-parse HEAD)"
+
+# shellcheck source=.builds/lib.sh
+. "$basedir/lib.sh"
+
+if ! commit_on_branch "$GIT_COMMIT" "master"; then
+  GORELEASER_FLAGS="--snapshot --skip-publish"
+fi
+
+# we want word splitting here
+# shellcheck disable=SC2086
+~/go/bin/goreleaser $GORELEASER_FLAGS --rm-dist
+
+# sort the hashes of the built binaries in a reliable (if derpy) way
+find dist -executable -type f -exec md5sum '{}' \; | rev | sort | rev
