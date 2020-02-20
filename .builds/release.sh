8913393cfd2d36e5f2148f75f004534d9ac9b4b3
Chris Waldon <christopher.waldon.dev@gmail.com>
2020-02-19 21:54:23 -0500

    ci/cd: ensure snapshot build on untagged master branch

--- .builds/release.sh
+++ .builds/release.sh
@@ -9,13 +9,20 @@ GIT_COMMIT="$(git rev-parse HEAD)"
 # shellcheck source=.builds/lib.sh
 . "$basedir/lib.sh"
 
-if ! commit_on_branch "$GIT_COMMIT" "master"; then
+# if we're not on master or master isn't tagged
+if ! commit_on_branch "$GIT_COMMIT" "master" || ! git describe --tags --exact-match HEAD; then
   GORELEASER_FLAGS="--snapshot --skip-publish"
+fi
+
+if command -v goreleaser ; then
+    goreleaser_path=$(command -v goreleaser)
+elif find "$HOME/go/bin" -executable -type f -name goreleaser; then
+    goreleaser_path="$HOME/go/bin/goreleaser"
 fi
 
 # we want word splitting here
 # shellcheck disable=SC2086
-~/go/bin/goreleaser $GORELEASER_FLAGS --rm-dist
+"$goreleaser_path" $GORELEASER_FLAGS --rm-dist
 
 # sort the hashes of the built binaries in a reliable (if derpy) way
 find dist -executable -type f -exec md5sum '{}' \; | rev | sort | rev
