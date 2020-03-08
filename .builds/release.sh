1da32d009504952182ecd04ac4856a24bee32d74
Chris Waldon <christopher.waldon.dev@gmail.com>
2020-03-08 17:45:45 -0400

    ci/cd: try to fix auto-release on tags for srht

--- .builds/release.sh
+++ .builds/release.sh
@@ -3,23 +3,23 @@
 set -x
 basedir=$(dirname "$(realpath "$0")")
 
-# determine current commit
-GIT_COMMIT="$(git rev-parse HEAD)"
-
 # shellcheck source=.builds/lib.sh
 . "$basedir/lib.sh"
 
 # if we're not on master or master isn't tagged
-ON_MASTER=0
-if ! commit_on_branch "$GIT_COMMIT" "master" || ! git describe --tags --exact-match HEAD; then
-  GORELEASER_FLAGS="--snapshot --skip-publish"
-  ON_MASTER=1
+PUBLISH_RELEASE=0
+if git describe --tags --exact-match HEAD; then
+  PUBLISH_RELEASE=1
 fi
 
 if command -v goreleaser ; then
     goreleaser_path=$(command -v goreleaser)
 elif find "$HOME/go/bin" -executable -type f -name goreleaser; then
     goreleaser_path="$HOME/go/bin/goreleaser"
+fi
+
+if [ "$PUBLISH_RELEASE" -eq 1 ]; then
+  GORELEASER_FLAGS="--snapshot --skip-publish"
 fi
 
 # we want word splitting here
@@ -30,7 +30,7 @@ fi
 find dist -executable -type f -exec sha256sum '{}' \; | rev | sort | rev
 
 # check if we're on master and on a tag
-if [ "$ON_MASTER" -eq 1 ] && git describe --exact-match HEAD > /dev/null 2>&1; then
+if [ "$PUBLISH_RELEASE" -eq 1 ]; then
     # erase the non-tarred directories from disk
     find dist -type d --exec rm -rf '{}' \;
 
